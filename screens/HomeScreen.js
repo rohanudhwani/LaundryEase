73e53131f01f8dbfe4edda2ca8fc414799894ed9
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-12-21 13:04:23 +0530

    added Carousel

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Alert, Pressable, Image, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import * as Location from 'expo-location'
+import { Feather, MaterialIcons } from '@expo/vector-icons'
+import Carousel from '../components/Carousel'
+
 
 
 
@@ -58,7 +61,24 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text>{displayCurrentAddress}</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', padding:10 }}>
+            <MaterialIcons name="location-on" size={30} color="#fd5c63" />
+            <View>
+                <Text style={{ fontSize: 18, fontWeight: "600"}}>Home</Text>
+                <Text>{displayCurrentAddress}</Text>
+            </View>
+
+            <Pressable style={{marginLeft:'auto', marginRight:7}}>
+                <Image style={{width: 50, height:50, borderRadius:20}} source={{uri:"https://lh3.googleusercontent.com/-PyCd43npZm8/AAAAAAAAAAI/AAAAAAAAAAA/AFNEGgJLlzktrC8P2knXBTsPxdbCeg-OCA/photo.jpg?sz=46"}} />
+            </Pressable>
+        </View> 
+
+        <View style={{padding:10, margin:10, flexDirection:"row", alignItems:"center", justifyContent:'space-between', borderWidth: 0.8, borderColor: "#C0C0C0", borderRadius:7}}>
+            <TextInput placeholder="Search for items or More" />
+            <Feather name="search" size={24} color="#fd5c63"/>
+        </View>
+
+        <Carousel />
     </SafeAreaView>
   )
 }
